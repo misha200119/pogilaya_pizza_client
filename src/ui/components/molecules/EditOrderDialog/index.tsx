f1f0736e9e1d3fe24f8731e9c6f07f30c8103f25
Misha Kravtsov <aniahucm@gmail.com>
2022-10-23 17:28:02 +0300

    added fields mapper to table edit order

--- src/ui/components/molecules/EditOrderDialog/index.tsx
+++ src/ui/components/molecules/EditOrderDialog/index.tsx
@@ -26,84 +26,47 @@ export const EditOrderDialog: FC<Props> = memo(({
     comment, coupon, date, email, entrance, flat, floor, house, intercomCode, isPaid, nameField, paymentType, phoneNumberField, selectedDeliveryType, street, totalCost,
   } = order;
 
-  const [comment_, set_comment] = useState(comment);
-  const [coupon_, setCoupon_] = useState(comment);
-  const [date_, setDate_] = useState(date);
-  const [email_, setEmail_] = useState(email);
-  const [entrance_, setEntrance_] = useState(entrance);
-  const [flat_, setFlat_] = useState(flat);
-  const [floor_, setFloor_] = useState(floor);
-  const [house_, setHouse_] = useState(house);
-  const [intercomCode_, setIntercomCode_] = useState(intercomCode);
-  const [isPaid_, setIsPaid_] = useState(isPaid);
-  const [nameField_, setNameField_] = useState(nameField);
-  const [paymentType_, setPaymentType_] = useState(paymentType);
-  const [phoneNumberField_, setPhoneNumberField_] = useState(phoneNumberField);
-  const [selectedDeliveryType_, setSelectedDeliveryType_] = useState(selectedDeliveryType);
-  const [street_, setStreet_] = useState(street);
-  const [totalCost_, setTotalCost_] = useState(totalCost);
-
-  // eslint-disable-next-line no-console
-  console.log(order, 'order');
+  const fields = {
+    comment: useState(comment),
+    coupon: useState(coupon),
+    date: useState(date),
+    email: useState(email),
+    entrance: useState(entrance),
+    flat: useState(flat),
+    floor: useState(floor),
+    house: useState(house),
+    intercomCode: useState(intercomCode),
+    isPaid: useState(isPaid),
+    nameField: useState(nameField),
+    paymentType: useState(paymentType),
+    phoneNumberField: useState(phoneNumberField),
+    selectedDeliveryType: useState(selectedDeliveryType),
+    street: useState(street),
+    totalCost: useState(totalCost),
+  };
 
   return (
     <Dialog open={isOpen} onClose={handleClose}>
       <DialogTitle>Subscribe</DialogTitle>
       <DialogContent>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Email Address"
-          type="email"
-          fullWidth
-          variant="standard"
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Email Address"
-          type="email"
-          fullWidth
-          variant="standard"
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Email Address"
-          type="email"
-          fullWidth
-          variant="standard"
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Email Address"
-          type="email"
-          fullWidth
-          variant="standard"
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Email Address"
-          type="email"
-          fullWidth
-          variant="standard"
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Email Address"
-          type="email"
-          fullWidth
-          variant="standard"
-        />
+        {(Object.keys(fields) as Array<keyof typeof fields>).map((key) => {
+          const field = fields[key];
+
+          return (
+            <TextField
+              key={key}
+              autoFocus
+              margin="dense"
+              id={key}
+              label={key}
+              type="text"
+              fullWidth
+              variant="standard"
+              value={field[0]}
+              onChange={({ target }) => field[1](target.value as any)}
+            />
+          );
+        })}
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Cancel Edit</Button>
