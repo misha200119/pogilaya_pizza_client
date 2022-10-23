4e526dd879197ddc6bb9968bef40ad5611801d11
Misha Kravtsov <aniahucm@gmail.com>
2022-10-23 17:01:54 +0300

    working on editing order dialog form

--- src/ui/components/molecules/EditOrderDialog/index.tsx
+++ src/ui/components/molecules/EditOrderDialog/index.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
-  Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField,
+  Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField,
 } from '@mui/material';
-import React, { FC, memo } from 'react';
+import React, { FC, memo, useState } from 'react';
 import IOrder from '../../../../utils/models/order/IOrder';
 
 interface Props {
   isOpen: boolean;
+  order: IOrder;
   handleClose: () => void;
   handleDelete: () => void;
   handleSave: (newOrder: IOrder) => void;
@@ -16,15 +19,82 @@ export const EditOrderDialog: FC<Props> = memo(({
   handleClose,
   handleSave,
   handleDelete,
+  order,
 }) => {
+  const {
+    // eslint-disable-next-line max-len
+    comment, coupon, date, email, entrance, flat, floor, house, intercomCode, isPaid, nameField, paymentType, phoneNumberField, selectedDeliveryType, street, totalCost,
+  } = order;
+
+  const [comment_, set_comment] = useState(comment);
+  const [coupon_, setCoupon_] = useState(comment);
+  const [date_, setDate_] = useState(date);
+  const [email_, setEmail_] = useState(email);
+  const [entrance_, setEntrance_] = useState(entrance);
+  const [flat_, setFlat_] = useState(flat);
+  const [floor_, setFloor_] = useState(floor);
+  const [house_, setHouse_] = useState(house);
+  const [intercomCode_, setIntercomCode_] = useState(intercomCode);
+  const [isPaid_, setIsPaid_] = useState(isPaid);
+  const [nameField_, setNameField_] = useState(nameField);
+  const [paymentType_, setPaymentType_] = useState(paymentType);
+  const [phoneNumberField_, setPhoneNumberField_] = useState(phoneNumberField);
+  const [selectedDeliveryType_, setSelectedDeliveryType_] = useState(selectedDeliveryType);
+  const [street_, setStreet_] = useState(street);
+  const [totalCost_, setTotalCost_] = useState(totalCost);
+
+  // eslint-disable-next-line no-console
+  console.log(order, 'order');
+
   return (
     <Dialog open={isOpen} onClose={handleClose}>
       <DialogTitle>Subscribe</DialogTitle>
       <DialogContent>
-        <DialogContentText>
-          To subscribe to this website, please enter your email address here. We
-          will send updates occasionally.
-        </DialogContentText>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="standard"
+        />
         <TextField
           autoFocus
           margin="dense"
--- src/ui/components/molecules/OrdersTable/index.tsx
+++ src/ui/components/molecules/OrdersTable/index.tsx
@@ -9,6 +9,7 @@ import {
   Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow,
 } from '@mui/material';
 import IOrder from '../../../../utils/models/order/IOrder';
+import { EditOrderDialog } from '../EditOrderDialog';
 
 interface Column {
   id: 'number' | 'paymentType' | 'cost' | 'isPaid';
@@ -79,83 +80,102 @@ export const OrdersTable: FC<Props> = memo(({ orders }) => {
   const handleEditRowClick = useCallback((targetObject: IOrder) => {
     setEditableObject(targetObject);
     setIsEditablePopupOpen(true);
-    // eslint-disable-next-line no-console
-    console.log(targetObject);
   }, [setEditableObject, setIsEditablePopupOpen]);
 
-  return (
-    <Paper
-      sx={{
-        width: '100%',
-        overflow: 'hidden',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-    >
-      <TableContainer sx={{ height: '75%' }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody sx={{ overflow: 'auto' }}>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow
-                    hover
-                    role="checkbox"
-                    tabIndex={-1}
-                    key={row.number}
-                    sx={{ cursor: 'pointer' }}
-                    onClick={() => handleEditRowClick(row._object)}
-                  >
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      const isValueBoolean = typeof value === 'boolean';
+  const handleEditDialogClose = useCallback(() => {
+    setIsEditablePopupOpen(false);
+  }, [setIsEditablePopupOpen]);
 
-                      return (
-                        <TableCell key={column.id} align={column.align}>
-                          {isValueBoolean ? (
-                            <Checkbox
-                              color="primary"
-                              checked={value as boolean}
-                              readOnly
-                            />
-                          ) : column.format && typeof value === 'number' ? (
-                            column.format(value)
-                          ) : (
-                            value
-                          )}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+  const handleOnDeleteEditing = useCallback(() => {}, []);
+
+  const handleOnSaveEditing = useCallback((editedObject: IOrder) => {
+
+  }, []);
+
+  return (
+    <>
+      <Paper
+        sx={{
+          width: '100%',
+          overflow: 'hidden',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <TableContainer sx={{ height: '75%' }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow
+                      hover
+                      role="checkbox"
+                      tabIndex={-1}
+                      key={row.number}
+                      sx={{ cursor: 'pointer' }}
+                      onClick={() => handleEditRowClick(row._object)}
+                    >
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        const isValueBoolean = typeof value === 'boolean';
+
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {isValueBoolean ? (
+                              <Checkbox
+                                color="primary"
+                                checked={value as boolean}
+                                readOnly
+                              />
+                            ) : column.format && typeof value === 'number' ? (
+                              column.format(value)
+                            ) : (
+                              value
+                            )}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      {isEditablePopupOpen && editableObject && (
+        <EditOrderDialog
+          order={editableObject}
+          isOpen={isEditablePopupOpen}
+          handleClose={handleEditDialogClose}
+          handleSave={handleOnSaveEditing}
+          handleDelete={handleOnDeleteEditing}
+        />
+      )}
+    </>
   );
 });
